eb2a293faf440b0877c6c64f85e518af2ba9aad7
Diego Godoy <diegogodoy@outlook.com>
2018-06-20 13:23:50 -0300

    implementação inicial para tableau

--- PO.cpp
+++ PO.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "tableau.hpp"
 
 int main(){
+	tableau<float> t({2, 3, 1},
+					{
+						{1, 1, 1},
+						{2, 1, -1},
+						{3, 2, -1}
+					},
+					{40,20,30}
+					);
+	t.print();
 	return 0;
 };
---
+++ tableau.hpp
@@ -0,0 +1,71 @@
+#pragma once
+#include <vector>
+#include <iostream>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+template<class N = float>
+//N é o tipo númerico.
+class tableau{
+	vector<vector<N>> data;
+		public:
+	tableau(vector<N> Z, //Z são as constantes da função objetiva
+			vector<vector<N>> A, //A são as constantes da  matriz de restrições
+			vector<N> b, //b é o lado direito das restrições
+			//vector<T4> lowerBound,
+			bool maximize = true
+			){
+		//primeiramente testarei se as dimensões são válidas
+		if( A.size() != b.size()) throw;
+
+		//lowerBound = vector<T4>(Z.size());
+
+		auto const columns = Z.size() + A.size() + 2;
+		auto const lines = A.size() + 1;
+
+		//fill tableau with 0's
+		data = vector<vector<N>>(lines);
+		for(auto& line : data)
+			for(size_t i = 0; i < columns; ++i)
+				line.push_back(N());
+
+		//Z
+		data[0][0] = maximize ? 1 : -1;
+		for(size_t i  = 1; i < Z.size()+1; ++i){
+			data[0][i] = -Z[i-1];
+		}
+
+		//A
+		for(size_t line = 1; line < Z.size()+1; ++line){
+			for(size_t column = 1; column < Z.size()+1; ++column){
+				data[line][column] = A[line-1][column-1];
+			}
+		}
+
+		//S
+		for(size_t i = Z.size()+1; i < Z.size()+A.size()+1; ++i){
+			data[i-Z.size()][i] = 1;
+		}
+
+		//b
+		for(size_t i = 1; i < b.size()+1; ++i){
+			data[i][columns-1] = b[i-1];
+		}
+	}
+
+	tableau() {
+	}
+
+	void print(){
+		for(const auto& line: data){
+			for(const auto& var: line)
+				cout << var << "\t";
+			cout << endl;
+		}
+	};
+	//TODO: add caso de Minimização.
+	//TODO: alterar para permitir simplex de duas fases
+	//TODO: add caso para restrições <=, = e >=. Aka var auxiliar
+};
